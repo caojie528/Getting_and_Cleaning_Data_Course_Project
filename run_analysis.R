62c68cb254cd09ad5cae3c8310fa649e3af5ea9f
caojie528 <43012478+caojie528@users.noreply.github.com>
2020-11-02 12:50:08 -0500

    Added run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,62 @@
+# Getting and Cleaning Data - Course Project
+# Author: Jie Cao
+# Last updated on: Nov 2, 2020
+
+# This R script performs following: 
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation 
+#    for each measurement.
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names.
+# 5. From the data set in step 4, creates a second, 
+#    independent tidy data set with the average of each variable 
+#    for each activity and each subject.
+
+# Load packages
+library(dplyr)
+library(tidyr)
+
+# Read training data
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+Y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+subj_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+# Read test data
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+Y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subj_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+# Read feature names
+feature_names <- read.table("./UCI HAR Dataset/features.txt", 
+                       col.names = c("index", "featurename"))
+feature_names$featurename <- gsub("[()]", "", feature_names$featurename)
+feature_names$featurename <- gsub("-", "_", feature_names$featurename)
+
+# Read activity labels
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt",
+                              col.names = c("class", "activity"))
+
+# Merges training and test, add descriptive names for features
+train <- cbind(subj_train, Y_train, X_train)
+names(train) <- c("subject", "activity", as.character(feature_names$featurename))
+test <- cbind(subj_test, Y_test, X_test)
+names(test) <- c("subject", "activity", as.character(feature_names$featurename))
+combined <- rbind(train, test)
+
+# Select wanted features (mean and standard deviation measurements)
+selected_feats <- grep("_mean_|_std_", names(train))
+selected_combined <- combined[, c(1, 2, selected_feats)]
+
+# Use descriptive names for activity (class)
+selected_combined$activity <- factor(selected_combined$activity, 
+                                     levels = activity_labels$class, 
+                                     labels = activity_labels$activity)
+
+# Create a tidy data with the average of each variable 
+# for each activity and each subject
+avg_feats <- selected_combined %>% 
+  group_by(activity, subject) %>% 
+  summarise_all(list(mean = mean))
+
+# Save the generated tidy data
+write.table(avg_feats, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE)
